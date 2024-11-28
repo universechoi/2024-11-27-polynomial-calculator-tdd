c0b8a4012ee8a79f87c26f8cd4796adde6216016
universechoi <skye.of.universe@gmail.com>
2024-11-28 16:45:55 +0900

    feat: add

--- src/main/java/com/ll/Calculator.java
+++ src/main/java/com/ll/Calculator.java
@@ -1,7 +1,15 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
     public int run(String input) {
-        return 8;
+        List<String> inputs = List.of(input.split("\s"));
+
+        int num1 = Integer.parseInt(inputs.get(0));
+        int num2 = Integer.parseInt(inputs.get(2));
+
+        return num1 + num2;
     }
 }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,5 +4,7 @@ package com.ll;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.run("3 + 5");
     }
 }
