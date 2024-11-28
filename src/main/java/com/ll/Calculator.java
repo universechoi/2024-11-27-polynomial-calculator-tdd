d6527617f8037151bd48ec853096a05a72ec4368
universechoi <skye.of.universe@gmail.com>
2024-11-28 16:55:05 +0900

    feat: min

--- src/main/java/com/ll/Calculator.java
+++ src/main/java/com/ll/Calculator.java
@@ -1,15 +1,34 @@
 package com.ll;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
     public int run(String input) {
         List<String> inputs = List.of(input.split("\s"));
 
+        int answer = 0;
+
         int num1 = Integer.parseInt(inputs.get(0));
         int num2 = Integer.parseInt(inputs.get(2));
 
-        return num1 + num2;
+        String operator = inputs.get(1);
+
+        switch (operator) {
+            case ("+"):
+                answer = add(num1, num2);
+                break;
+            case ("-"):
+                answer = min(num1, num2);
+                break;
+        }
+        return answer;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int min(int a, int b) {
+        return a - b;
     }
 }
